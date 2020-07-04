7d0cad6a1e8850b2b6fb94c6b7c6c5cfdd4793d2
oleks11redka <redka.aleksey@gmail.com>
2020-07-04 12:07:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Person from './components/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    name: 'Alex',
+    age: 23
+  };
+
+  ShowAlert = (name, age) => {
+    alert(`My name is ${name}, my age is ${age}`)
+  }
+
+  GetYearOlder = () => {
+    this.setState({
+      age: this.state.age + 1
+    })
+  }
+
+
+  render() {
+    let {name, age} = this.state
+    return (
+        <Person name={name} age={age} showAlert={this.ShowAlert} getYearOlder={this.GetYearOlder} />
+    )
+  }
 }
-
-export default App;
